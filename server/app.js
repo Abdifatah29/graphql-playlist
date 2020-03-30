927b4491e245dd128a3dfe0fea0df9926801a388
Abdiftah29 <abdi.hassannoor01@gmail.com>
2020-03-30 17:00:43 +0200

    tested queries in graphiql

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const app = express();
 //this function will handle grapghql request
 app.use('/graphql', graphqlHTTP({
     //this schema will tell express how how diagram will look
-    schema
+    schema,
+    graphiql: true
 }));
 app.listen(3000, () => {
     console.log('Now listening for requests on port 30000');
