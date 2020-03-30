25e759d93605bc0171ed3708356badd0d9ff93b3
Abdiftah29 <abdi.hassannoor01@gmail.com>
2020-03-30 14:59:57 +0200

    GraphQL schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
+//a convension for express-graghql
+const graphqlHTTP = require('express-graphql');
 const app = express();
 
+//this function will handle grapghql request
+app.use('/graphql', graphqlHTTP({
+    //this scheme will tell express how how diagram will look
+    
+}));
 app.listen(3000, () => {
     console.log('Now listening for requests on port 30000');
 })
