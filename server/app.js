3d3497a16505576d68b40a6f680f98cdfa096ab6
Abdiftah29 <abdi.hassannoor01@gmail.com>
2020-03-30 15:14:09 +0200

    root query

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 //a convension for express-graghql
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 
 //this function will handle grapghql request
 app.use('/graphql', graphqlHTTP({
-    //this scheme will tell express how how diagram will look
-    
+    //this schema will tell express how how diagram will look
+    schema
 }));
 app.listen(3000, () => {
     console.log('Now listening for requests on port 30000');
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,4 +9,24 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
+});
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //get data from db/other source
+                args.id
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
