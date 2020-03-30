000f169c1eed658379315c832403ccebb6d0c507
Abdiftah29 <abdi.hassannoor01@gmail.com>
2020-03-30 18:12:55 +0200

    connected to db

--- server/app.js
+++ server/app.js
@@ -2,7 +2,18 @@ const express = require('express');
 //a convension for express-graghql
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 const app = express();
+
+//connect to Mlab db
+// mongoose.connect('mongodb+srv://afrah:afrah29@abdifatah29-k2ek3.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.createConnection('mongodb+srv://afrah:afrah29%40abdifatah29-k2ek3.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
+console.log('DB Connected');})
+
+//
+
+
 
 //this function will handle grapghql request
 app.use('/graphql', graphqlHTTP({
